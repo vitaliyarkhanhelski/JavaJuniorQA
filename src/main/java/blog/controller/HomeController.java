ee03e1b6ffeb84cdc90750f2fe3c75130af528a4
Vitaliy Arkhanhelski <vit667@gmail.com>
2020-09-19 00:10:36 +0200

    bigger update, better functionality - fixed bug findByFavorites and frontend status navigation

--- src/main/java/blog/controller/HomeController.java
+++ src/main/java/blog/controller/HomeController.java
@@ -23,7 +23,7 @@ public class HomeController {
     @GetMapping
     public String findAll(ModelMap map) {
         map.put("letters", MessageService.letters);
-        map.put("page","c");
+        map.put("status", "Main");
 
         List<Message> list = messageService.findAll();
 
@@ -37,6 +37,8 @@ public class HomeController {
     @GetMapping("/showFavorites")
     public String showFavorites(ModelMap map) {
         map.put("letters", MessageService.letters);
+        map.put("page", "f");
+        map.put("status", "Favorites");
         List<Message> list = messageService.findAllFavorites();
         Collections.sort(list, new SortByCharAndName());
         map.put("messages", list);
@@ -47,8 +49,10 @@ public class HomeController {
 
 
     @GetMapping("/favorite")
-    public String isFavorite(ModelMap map, @RequestParam("messageId") int id) {
+    public String isFavorite(ModelMap map, @RequestParam("messageId") int id, @RequestParam("page") String page) {
         messageService.checkFavorite(id);
+        if (page.equals("f")) return "redirect:/showFavorites";
+        if (page.equals("c")) return "redirect:/showToComplete";
         return "redirect:";
     }
 
@@ -56,6 +60,8 @@ public class HomeController {
     @GetMapping("/showToComplete")
     public String showToComplete(ModelMap map) {
         map.put("letters", MessageService.letters);
+        map.put("page", "c");
+        map.put("status", "To Complete");
         List<Message> list = messageService.findAllToComplete();
         Collections.sort(list, new SortByCharAndName());
         map.put("messages", list);
@@ -66,19 +72,21 @@ public class HomeController {
 
 
     @GetMapping("/complete")
-    public String isComplete(ModelMap map, @RequestParam("messageId") int id) {
+    public String isComplete(ModelMap map, @RequestParam("messageId") int id, @RequestParam("page") String page) {
         messageService.checkComplete(id);
+        if (page.equals("c")) return "redirect:/showToComplete";
+        if (page.equals("f")) return "redirect:/showFavorites";
         return "redirect:";
     }
 
 
     @GetMapping("/find")
     public String findByChar(ModelMap map, @RequestParam("info") String info) {
-
         map.put("letters", MessageService.letters);
 
         if (info.equals("all")) return "redirect:";
         Character character = info.charAt(0);
+        map.put("status", character);
         List<Message> list = messageService.findByChar(character);
 
         if (list.isEmpty()) map.put("noRecords", "No Records Found");
@@ -88,12 +96,11 @@ public class HomeController {
     }
 
 
-
     @GetMapping("/findWord")
     public String findByWord(ModelMap map, @RequestParam("word") String word) {
 
         map.put("letters", MessageService.letters);
-
+        map.put("status", "Search Results");
 //        if (info.equals("all")) return "redirect:";
 //        Character character = info.charAt(0);
         List<Message> list = messageService.findByWord(word);
@@ -109,9 +116,12 @@ public class HomeController {
     public String findFavoritesByChar(ModelMap map, @RequestParam("info") Character info) {
 
         map.put("letters", MessageService.letters);
+        map.put("status", info);
 
-        map.put("messages", messageService.findFavoritesByChar(info));
-        if (messageService.findFavoritesByChar(info).isEmpty()) map.put("noRecords", "No Records Found");
+        List<Message> list = messageService.findFavoritesByChar(info);
+        if (list.isEmpty()) map.put("noRecords", "No Records Found");
+        else map.put("messages", list);
+
         return "index";
     }
 
